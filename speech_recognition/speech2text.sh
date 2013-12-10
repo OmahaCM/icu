6cf9e3f76af199456ce29b8ed39269f0322a3842
Rob Townley <rob.townley+ocm@gmail.com>
2013-12-10 19:21:22 +0000

    SpeechRecognition submodule

--- speech_recognition/speech2text.sh
+++ speech_recognition/speech2text.sh
@@ -1,16 +1,42 @@
-#!/bin/bash -x
+#!/bin/bash          
+DEBUG=0
+DURATION=--duration=7
+AUDIOFORMAT=--format=cd         # U8         # cd
+REMOVEOLDFILES=1
+REMOVECONFIDENCE=1
+PRINTOUT=
 
-arecord -D 'plughw:1,0' -q -f cd -t wav | ffmpeg -loglevel panic -y -i - -ar 16000 -acodec flac file.flac 
+if [[ 1 -eq $REMOVEOLDFILES ]]; then
+  rm file.flac stt.json stt.txt > /dev/null 2>&1
+fi;
 
-echo done recording, lets upload.
-#wget -q -U 'Mozilla/5.0' --post-file arecord.wav \
-     --header 'Content-Type: audio/x-wav; rate=8000;' \
-     -O stt-wave.txt \
-     'http://www.google.com/speech-api/v1/recognize?lang=en-us&client=chromium'  
+arecord --mmap --start-delay=0 -D 'plughw:1,0' $DURATION -q $AUDIOFORMAT -t wav | ffmpeg -loglevel panic -y -i - -ar 16000 -acodec flac file.flac > /dev/null 2>&1
+if [[ 1 -le $DEBUG ]]; then 
+  echo done recording, lets upload. 
+fi;
+
+if [[ 0 -eq 1 ]]; then
+  wget -q -U 'Mozilla/5.0' --post-file arecord.wav \
+   --header 'Content-Type: audio/x-wav; rate=8000;' \
+   -O stt-wave.txt \
+   'http://www.google.com/speech-api/v1/recognize?lang=en-us&client=chromium'  
+fi;
 
 wget -q -U 'Mozilla/5.0' --post-file file.flac \
      --header 'Content-Type: audio/x-flac; rate=16000' \
-     -O stt.txt \
+     -O stt.json \
      'http://www.google.com/speech-api/v1/recognize?lang=en-us&client=chromium'  
-echo done uploading
-#rm file.flac  > /dev/null 2>&1
+if [[ 1 -le $DEBUG ]]; then 
+  echo done uploading, getting stt.json.
+fi;
+
+if [[ 1 -eq $REMOVECONFIDENCE ]]; then
+  PRINTOUT=' $1 ';  #PRINTS JUST THE UTTERANCE
+  cat stt.json | cut -d: -f5-7 | sed -e 's/,\"confidence\":/=/' | sed -e 's/}]}//' | awk -F = '{ print $1 }'  > stt.txt
+
+else
+
+  PRINTOUT=' $2 = "$1" ';  #PRINTS CONFIDENCE of SPEECH REC UTTERANCE
+  cat stt.json | cut -d: -f5-7 | sed -e 's/,\"confidence\":/=/' | sed -e 's/}]}//' | awk -F = '{ print $PRINTOUT }'  |sort --reverse --numeric-sort --field-separator== --key=2 > stt.txt
+
+fi;
